743ede027d0aa23fc493d426cd54ceb3e4c96c96
Seena Fallah <seenafallah@gmail.com>
2019-04-30 02:43:25 +0430

    + Bug fixed

--- users/models.py
+++ users/models.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     first_name = models.CharField('First Name', max_length=50)
     last_name = models.CharField('Last Name', max_length=50)
-    approved = models.BooleanField('Approved', default=False)
 
     def __str__(self):
         return self.first_name + " " + self.last_name
